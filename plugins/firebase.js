24144f41b5f0bfd6383a9d84340d467a906ae7f0
yama-choki <lovepingpong0@gmail.com>
2021-10-11 14:52:38 +0900

    firebaseを８系にダウングレード

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,15 +3,17 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 if (!Firebase.apps.length) {
-  Firebase.initializeApp({
-    apiKey: 'AIzaSyA0dKkiJQPJWTZKRPRfrWVVJkbBT9-xSDc',
-    authDomain: 'chat-app-d6e59.firebaseapp.com',
-    projectId: 'chat-app-d6e59',
-    storageBucket: 'chat-app-d6e59.appspot.com',
-    messagingSenderId: '303487276515',
-    appId: '1:303487276515:web:2b19dfe1fdc1e752077cd6',
-    measurementId: 'G-2WYT6QJMVQ'
-  })
+  Firebase.initializeApp(
+    {
+      apiKey: 'AIzaSyA0dKkiJQPJWTZKRPRfrWVVJkbBT9-xSDc',
+      authDomain: 'chat-app-d6e59.firebaseapp.com',
+      projectId: 'chat-app-d6e59',
+      storageBucket: 'chat-app-d6e59.appspot.com',
+      messagingSenderId: '303487276515',
+      appId: '1:303487276515:web:2b19dfe1fdc1e752077cd6',
+      measurementId: 'G-2WYT6QJMVQ'
+    }
+  )
 }
 
 export default (context, inject) => {
